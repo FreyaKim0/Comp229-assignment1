5b717aa1141707603f4e027c0b2f5dce899f40b8
Freya <64510536+FreyaKim0@users.noreply.github.com>
2021-06-17 00:08:30 -0400

    update index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,18 @@ let indexController = require('../controllers/index');
 
 /* Get home page */
 router.get('/',indexController.displayHomePage);
+router.get('/home',indexController.displayHomePage);
+
+/* GET service page. */
+router.get('/service',indexController.displayServicePage);
+
+/* GET project page. */
+router.get('/project',indexController.displayProjectPage);
+
+/* GET about page. */
+router.get('/about',indexController.displayAboutPage);
+
+/* GET contact page. */
+router.get('/contact',indexController.displayContactPage);
 
 module.exports = router;
